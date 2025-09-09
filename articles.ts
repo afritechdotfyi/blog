71a1869d7d1a179132bd2d3aa5e5cad54871b7b7
laudebugs <laudebugs@gmail.com>
2025-09-09 03:10:52 -0400

    update

--- articles.ts
+++ articles.ts
@@ -2,7 +2,7 @@ import { TArticle } from "./t-article";
 
 export const articles: TArticle[] = [
   {
-    slug: "1",
+    slug: "why-we-are-building-afritech",
     title: "Why We Are Building Afritech",
     publishedAt: '2025-09-09',
     file: './articles/why-we-are-building-afritech.md'
